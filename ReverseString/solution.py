be3c2a9ab63db4c919654300db4eb440704ce80d
tvictor <ateniolatobi@gmail.com>
2020-06-19 23:30:53 +0100

    Added ReverseString

---
+++ ReverseString/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start =  0
+        end = len(s) - 1
+        
+        while start <= end:
+            # print((start,end))
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -=1
+            
+            
